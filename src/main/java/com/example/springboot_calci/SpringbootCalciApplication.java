34ba3e9c08e9c4bcadfbeb386eb3fb17c15f0605
Ragavi <ragavi9003@mountzion.ac.in>
2025-02-16 13:05:26 +0530

    first commit

---
+++ src/main/java/com/example/springboot_calci/SpringbootCalciApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_calci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCalciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCalciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_calci/SpringbootCalciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_calci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCalciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
